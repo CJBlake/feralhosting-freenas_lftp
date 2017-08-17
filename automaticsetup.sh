82597527fd6731e7409fd1065032aafbb49e1c57
Chris Gornall Blake <CJBlake@users.noreply.github.com>
2017-08-17 18:17:29 +1000

    Update automaticsetup.sh

--- automaticsetup.sh
+++ automaticsetup.sh
@@ -1,41 +1,58 @@
 #!/bin/bash
-jail="jailname" 
-uname="username"
-uid="uid of freenas share user"
-grpname="group name"
-flogin="username for feral ftp"
-fpass="password for feral ftp"
-fhost="server.feralhosting.com"
-fremote_dir='~/folder/you/want/to/copy'
-flocal_dir="/folder/you/mounted/to/jail"
 
-jexec "$jail" tcsh
-pkg clear -y
-pkg update -y
+###### Get User unput for varibles and check with user they are correct
+while true; do
+    echo "Enter desired username for lftp service"
+	read uname
+	echo "Enter desired UID of user"
+	read uid
+	echo "Enter Desired groupname for lftp service"
+	read grpname
+	echo "Enter Username to Seedbox Server for SFTP"
+	read flogin
+	echo "Enter Password to Seedbox Server for SFTP"
+	read fpass
+	echo "Enter homename to Seedbox Server eg. monkey.feralhosting.com "
+	read fhost
+	echo "Enter the remote directory path you want to copy"
+	echo "eg. ~/folder/you/want/to/copy"
+	read fremote_dir
+	echo "Enter the local path you want to copy to"
+	echo "eg. /folder/you/mounted/to/jail"
+	read flocal_dir
+
+	echo "Are these values correct\?"
+	echo "LFTP USER = $uname"
+	echo "LFTP USER UID = $uid"
+	echo "GROUP NAME = $grpname"
+	echo "Seedbox Login = $flogin"
+	echo "Seedbox Password = $fpass"
+	echo "Seedbox Hostname = $fhost"
+	echo "Remote Directory = $fremote_dir"
+	echo "Local Directory = $flocal_dir"
+	read -p "Are these values correct?" yn
+	case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+pkg clean -y
+pkg update -f
 pkg upgrade -y
-pkg install nano -y
-pkg install bash -y
-pkg install lftp -y
-pkg intall wget -y
+pkg install -y nano
+pkg install -y bash
+pkg install -y lftp
+pkg install -y wget
 pw groupadd "$grpname"
-adduser 
-""$uname"
-"$uname"
-"$uid"
-"$grpname"
+pw useradd -m -s tcsh -u "$uid" -g "$grpname" -n "$uname"
+su "$uname" -c "mkdir ~/scripts"
 
-
-tcsh
-
-
-no
-no
-yes
-no"
-su "$uname"
-mkdir ~/scripts
-cd ~/scripts
-cat > syncdownloads.sh <<EOF1
+su "$uname" -c "cat > ~/scripts/syncdownloads.sh << 'ENDMASTER'
+$(
+###### The parameter substitution is on here
+cat <<INNERMASTER
 #!/bin/bash
 login="$flogin"
 pass="$fpass"
@@ -43,6 +60,10 @@ host="$fhost"
 remote_dir="$fremote_dir"
 local_dir="$flocal_dir"
 
+INNERMASTER
+
+###### No parameter substitution
+cat <<'INNERMASTER'
 base_name="$(basename "$0")"
 lock_file="/tmp/$base_name.lock"
 trap "rm -f $lock_file exit 0" SIGINT SIGTERM
@@ -62,13 +83,16 @@ EOF
     trap - SIGINT SIGTERM
     exit
 fi
-EOF1
-chmod 700 syncdownloads.sh # Make the script executable
-exit
+
+INNERMASTER
+)
+ENDMASTER"
+su "$uname" -c "chmod 700 ~/scripts/syncdownloads.sh" # Make the script executable
 mkdir ~/scripts
-cd ~/scripts
-cat >runsync.sh <<EOF2
+cat >~/scripts/runsync.sh <<EOF2
 #!/bin/bash
-su  "#uname" -c "bash ~/scripts/syncdownloads.sh";
+su  "$uname" -c "bash ~/scripts/syncdownloads.sh";
 EOF2
-chmod 700 runsync.sh
+chmod 700 ~/scripts/runsync.sh
+
+exit 0
