a2f37482ccfe9786576efb687c1dc24ddecfbb5b
Chris Gornall Blake <CJBlake@users.noreply.github.com>
2017-11-22 18:37:17 +1100

    Update automaticsetup.sh

--- automaticsetup.sh
+++ automaticsetup.sh
@@ -59,7 +59,10 @@ pass="$fpass"
 host="$fhost"
 remote_dir="$fremote_dir"
 local_dir="$flocal_dir"
-temp_dir="~/temp"
+temp_dir="/temp"
+upload_rate="0"
+download_rate="0"
+H=$(date +%H)
 
 INNERMASTER
 
@@ -67,23 +70,34 @@ INNERMASTER
 cat <<'INNERMASTER'
 base_name="$(basename "$0")"
 lock_file="/tmp/$base_name.lock"
-trap "rm -f $lock_file exit 0" SIGINT SIGTERM
+trap "rm -f $lock_file; exit 0" SIGINT SIGTERM
 if [ -e "$lock_file" ]
 then
     echo "$base_name is running already."
     exit
 else
     touch "$lock_file"
+    if (( 9 <= 10#$H && 10#$H < 24 )); then
+        upload_rate="125000"
+        download_rate="5000000"
+        echo "limit on"
+    else
+        upload_rate="0"
+        download_rate="0"
+        echo "limit off"
+    fi
     lftp -p 22 -u "$login","$pass" sftp://"$host" << EOF
     set sftp:auto-confirm yes
-    set mirror:use-pget-n 5
-    mirror -c -P5 "$remote_dir" "$temp_dir"
+    set net:limit-rate "$upload_rate":"$download_rate"
+    set mirror:use-pget-n 50
+    mirror -c -v -P3 --loop --Remove-source-dirs "$remote_dir" "$temp_dir"
     quit
 EOF
+    mv  -v /temp/* "$local_dir"
     rm -f "$lock_file"
-    mv  -v ~/temp/* "$local_dir"
     trap - SIGINT SIGTERM
     exit
+
 fi
 
 INNERMASTER
